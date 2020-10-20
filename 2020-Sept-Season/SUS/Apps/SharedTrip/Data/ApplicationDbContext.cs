62a6aa3eb2bc9dc9e3781aee882469dad5dad08d
Tsveti Zlateva <tsveti.zlateva@gmail.com>
2020-10-20 15:37:57 +0300

    Add SharedTrip app

---
+++ 2020-Sept-Season/SUS/Apps/SharedTrip/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿namespace SharedTrip
+{
+    using Microsoft.EntityFrameworkCore;
+    using SharedTrip.Data;
+
+    public class ApplicationDbContext : DbContext
+    { 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(DatabaseConfiguration.ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserTrip>().HasKey(x => new { x.TripId, x.UserId });
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Trip> Trips { get; set; }
+
+        public DbSet<UserTrip> UserTrips { get; set; }
+    }
+}
